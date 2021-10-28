9e81e987de06a9ec103aa53c7bc63005a4801732
Daniel <Daniel.Dichte@t-online.de>
2021-10-28 17:41:43 +0200

    mini bug

--- src/ImplServer.java
+++ src/ImplServer.java
@@ -71,6 +71,7 @@ public class ImplServer implements MessageService{
         } else {
             deliveryQueue.remove();
             deliveryQueue.add(newMessage);
+            counterQueue++;
         }
     }
 
